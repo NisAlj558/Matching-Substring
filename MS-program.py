733bab5b76d984d58959ed8a17923e6b1172e33a
Nisreen Mishal <89920071+NisAlj558@users.noreply.github.com>
2021-09-13 19:49:25 +0300

    Update MS-program.py

--- MS-program.py
+++ MS-program.py
@@ -1,1 +1,13 @@
-
+string =input('Entetr string :')
+start =input('Entetr start litter :')
+end =input('Entetr end litter :')
+def countSubstring(string,start,end) :
+    totalCount= 0
+    count= 0
+    for i in string :
+        if i==start :
+            count=count+1
+        if i==end :
+            totalCount=totalCount+count
+    return print('count:',totalCount)
+countSubstring(string,start,end)
